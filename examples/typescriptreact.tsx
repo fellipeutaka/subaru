bd05d77f6792cadc18f487a98769563c7bfbdf86
Fellipe Utaka <fellipeutaka@gmail.com>
2024-12-05 12:41:54 -0300

    feat: add example HTML and improve python support

--- examples/typescriptreact.tsx
+++ examples/typescriptreact.tsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const template = `${JSON.stringify({ something: 'else'})}`
+const template = `${JSON.stringify({ something: "else" })}`;
 
-const Component = () => 
+const Component = () => (
   <>
     <div>Something 1</div>
     <div>Something 2</div>
   </>
+);
 
-
-export const Main = () =>
+export const Main = () => (
   <div>
     <Component />
-      {template}
+    {template}
     <Component />
   </div>
+);
+
+import type { Metadata, ResolvingMetadata } from "next";
+
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = (await params).id;
+
+  // fetch data
+  const product = await fetch(`https://.../${id}`).then((res) => res.json());
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: product.title,
+    openGraph: {
+      images: ["/some-specific-page-image.jpg", ...previousImages],
+    },
+  };
+}
+
+export default function Page({ params, searchParams }: Props) {}
